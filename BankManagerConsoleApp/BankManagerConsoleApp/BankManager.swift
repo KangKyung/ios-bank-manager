dc56eca871e7103fdde72e00289992cbf280abce
kangkyung <rudgns0508@naver.com>
2021-05-04 17:39:48 +0900

    Fix: 은행원 3명모두 끝날때 까지 일멈춤현상 해결

--- BankManagerConsoleApp/BankManagerConsoleApp/BankManager.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/BankManager.swift
@@ -7,6 +7,21 @@
 
 import Foundation
 
+class Test: Operation {
+  private let queue: Queue<Customer>
+  private let banker: Banker?
+  
+  init(queue: Queue<Customer>, workableBanker banker: Banker?) {
+    self.queue = queue
+    self.banker = banker
+  }
+  
+  override func main() {
+    guard let firstElement = queue.front else { return }
+    banker?.process(firstElement)
+  }
+}
+
 final class BankManager {
   let operationQueue = OperationQueue()
   
@@ -28,16 +43,21 @@ final class BankManager {
   }
   
   private func process(grade: CustomerGrade, _ queue: Queue<Customer>) {
-    guard !bankers.isEmpty else {
-      operationQueue.waitUntilAllOperationsAreFinished()
-      return
-    }
+//    guard !bankers.isEmpty else {
+//      operationQueue.waitUntilAllOperationsAreFinished()
+//      return
+//    }
     
     let workableBanker = self.bankers.first?.value
     guard let counterNumber = workableBanker?.showCounterNumber() else {
       return
     }
-    self.bankers.removeValue(forKey: counterNumber)
+    
+    if !self.bankers.isEmpty {
+      self.bankers.removeValue(forKey: counterNumber)
+    }
+    
+//    self.bankers.removeValue(forKey: counterNumber)
     
     NotificationCenter.default.post(
       name: NSNotification.Name(rawValue: "completedCustomer"),
@@ -46,12 +66,33 @@ final class BankManager {
     
     currentTicketNumber += 1
     
-    operationQueue.addOperation {
-      guard let firstElement = queue.front else { return }
-      workableBanker?.process(firstElement)
-      self.bankers[counterNumber] = workableBanker
+    let test = Test(queue: queue, workableBanker: workableBanker)
+    test.completionBlock = {
+      guard let testtest = workableBanker else {
+        return
+      }
+      self.bankers[counterNumber] = testtest
       self.totalCompletedCustomer += 1
     }
+    
+    if queue.front?.showGrade() == CustomerGrade.vvip {
+      test.queuePriority = Operation.QueuePriority.veryHigh
+    } else if queue.front?.showGrade() == CustomerGrade.vip {
+      test.queuePriority = Operation.QueuePriority.high
+    } else {
+      test.queuePriority = Operation.QueuePriority.normal
+    }
+    
+ 
+    
+    operationQueue.addOperation(test)
+    
+//    operationQueue.addOperation {
+//      guard let firstElement = queue.front else { return }
+//      workableBanker?.process(firstElement)
+//      self.bankers[counterNumber] = workableBanker
+//      self.totalCompletedCustomer += 1
+//    }
     
   }
   
